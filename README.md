ae41a2f5a3bc3d90fe6d27b3949a7c397a87cf77
mali-28 <alihadees786@gmail.com>
2021-02-27 16:37:01 +0500

    THIS IS MY SECOND COMMIT

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my git learning project
+This is my git  project
---
+++ index.js
@@ -0,0 +1,8 @@
+let x = +prompt();
+let y = +prompt();
+let z;
+sum(x,y)
+function sum(a,b){
+    z=a+b;
+    document.write(z);
+}
